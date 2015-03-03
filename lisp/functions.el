549710279b44ad0b915ace8fbc3e69966c2f3032
Cody Canning <cocanning11@gmail.com>
2015-03-02 21:30:51 -0500

    edit find-user-init-file
    don't open in another window, open in current window instead

--- lisp/functions.el
+++ lisp/functions.el
@@ -19,7 +19,7 @@
 (defun find-user-init-file ()
   "Edit the `user-init-file', in another window."
   (interactive)
-  (find-file-other-window user-init-file))
+  (find-file user-init-file))
 
 (setq curr-theme nil)
 
