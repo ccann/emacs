0c9940a6e355cb59914f7dcbb245bea2ac6d782d
Cody Canning <cocanning11@gmail.com>
2015-07-20 13:04:56 -0400

    more refactor, add python fix clojure

--- init.el
+++ init.el
@@ -53,59 +53,46 @@
 ;;;;;;;;;;;;;
 ;; Loaders ;;
 ;;;;;;;;;;;;;
-;; Keep emacs custom-settings in separate file, for fuck's sake.
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
 (load (expand-file-name "functions.el" user-emacs-directory))
 
-
 ;;;;;;;;;;;;;;
 ;; Packages ;;
 ;;;;;;;;;;;;;;
-(use-package idle-highlight-mode
-  :ensure t)
+(use-package idle-highlight-mode)
 
 (use-package smart-tab
-  :init (bind-key "<tab>" 'hippie-expand read-expression-map)
-  :ensure t)
+  :init (bind-key "<tab>" 'hippie-expand read-expression-map))
+
+;; (use-package subword-mode :defer t :diminish subword-mode)
 
 (use-package exec-path-from-shell
-  :ensure t
   :config
   (when (memq window-system '(mac ns))
     (exec-path-from-shell-initialize)
     (exec-path-from-shell-copy-env "PYTHONPATH")))
 
 (use-package smex
-  :ensure t
   :bind (("M-x" . smex))
   :config (smex-initialize))  ;; smart meta-x (use IDO in minibuffer)
 
 (use-package ido
-  :ensure t
-  :demand t ;; hmm
+  :demand t
   :bind (("C-x b" . ido-switch-buffer))
   :init
   (setq ido-create-new-buffer 'always  ;; don't confirm when creating new buffers
         ido-enable-flex-matching t  ;; fuzzy matching
         ido-everywhere t  ;; tbd
         ido-case-fold t)  ;; ignore case
+
+  (use-package ido-ubiquitous :config (ido-ubiquitous-mode 1))
+  (use-package flx-ido :config (flx-ido-mode 1))
+  (use-package ido-vertical-mode :config (ido-vertical-mode 1))
   :config (ido-mode 1))
-
-(use-package ido-ubiquitous
-  :ensure t
-  :config (ido-ubiquitous-mode 1))
-
-(use-package flx-ido ;; better/faster matching
-  :ensure t
-  :config (flx-ido-mode 1))
-
-(use-package ido-vertical-mode
-  :ensure t
-  :config (ido-vertical-mode 1))
   
 (use-package magit
-  :ensure t
+  :diminish magit-auto-revert-mode
   :bind (("<f10>" . magit-status))
   :config
   (when ccann/is-osx
@@ -114,21 +101,19 @@
 (use-package rainbow-delimiters :defer t)
 (use-package rainbow-mode :defer t)
 (use-package company
+  :init
+  (use-package company-anaconda
+    :diminish anaconda-mode
+    :config (add-to-list 'company-backends 'company-anaconda))
   :defer t
   :diminish company-mode
   :config
   (setq company-idle-delay 0.4)
   (setq company-minimum-prefix-length 3))
 
+(use-package dired-details+ :defer t)
 
-(use-package company-anaconda :defer t)
-
-
-(use-package dired-details+
-  :defer t)
-
-(use-package uniquify
-  :init (setq uniquify-buffer-name-style 'forward))
+(use-package uniquify :init (setq uniquify-buffer-name-style 'forward))
 
 (use-package saveplace
   :init
@@ -148,7 +133,6 @@
 (use-package zone
   :defer t
   :config (zone-when-idle 300))
-
 
 (use-package flycheck
   :defer t
@@ -191,12 +175,11 @@
       :fringe-face 'flycheck-fringe-info))
 
 
-(use-package fill-column-indicator
-  :disabled t)
+(use-package fill-column-indicator :disabled t)
 
+(use-package indent-guide :disabled t)
 
-(use-package indent-guide
-  :disabled t)
+(use-package projectile :disabled t)
 
 (use-package highlight-symbol
   :init
@@ -205,22 +188,10 @@
   :bind
   (("C-h s" . highlight-symbol))
   :config
-  (highlight-symbol-mode 1)
-  (highlight-symbol-nav-mode 1))
-
-(use-package projectile
-  :disabled t)
-
-
-(use-package jedi
-  :bind (("C-c d" . jedi:show-doc)))
-(use-package hydra :defer t)
+  (highlight-symbol-mode 1))
+  
 (use-package hlinum :defer t)
-(use-package nyan-mode
-  :config (nyan-mode 1))
-(use-package key-chord :defer t)
-
-
+(use-package nyan-mode :config (nyan-mode 1))
 
 ;;;;;;;;;;;;;;;;;;
 ;; markup modes ;;
@@ -327,89 +298,85 @@
   (sp--update-override-key-bindings)
   :commands (smartparens-mode show-smartparens-mode))
   
-
 (use-package ess :defer t)
-(use-package elpy :defer t)
-;; (when (eq system-type 'darwin)
-;;   (setq elpy-rpc-python-command "/usr/local/bin/python"))
 
-;; ;; set the PYTHONPATH to be what the shell has
-;; (let ((path-from-shell (shell-command-to-string "echo $PYTHONPATH")))
-;;   (setenv "PYTHONPATH" (mapconcat 'identity (split-string path-from-shell) ":")
-;;     ))
+(use-package python-mode
+  :mode ("\\.py\\'" . python-mode)
+  :init
+  (setq-default python-indent-guess-indent-offset nil)
+  (setq-default python-indent-offset 4)
+  (setq python-fill-docstring-style 'pep-257-nn)
+  (setq python-check-command "flake8")
+  
+  (use-package elpy
+    :defer t
+    :diminish elpy-mode
+    :init
+    (setq elpy-rpc-backend "jedi")
+    (setq elpy-modules '(elpy-module-company
+                         elpy-module-eldoc
+                         elpy-module-sane-defaults))
+    (when ccann/is-osx
+      (setq elpy-rpc-python-command "/usr/local/bin/python")))
 
-;; ;; set the LD_LIBRARY_PATH to be what the shell has (necessary for naoqi)
-;; (let ((path-from-shell (shell-command-to-string "echo $LD_LIBRARY_PATH")))
-;;   (setenv "LD_LIBRARY_PATH" (mapconcat 'identity (split-string path-from-shell) ":")
-;;     ))
-
-;; (add-to-list 'auto-mode-alist '("\\.xar\\'" . python-mode))
-;; (add-hook 'after-init-hook 'global-company-mode)
-;; ;;(eval-after-load 'company '(add-to-list 'company-backends 'company-anaconda))
-
-;; ;; don't guess the indent offset
-;; (setq-default python-indent-guess-indent-offset nil)
-;; ;; just use 4 spaces
-;; (setq-default python-indent-offset 4)
-
-;; (add-hook 'python-mode-hook (lambda ()
-;;                               (elpy-enable)
-;;                               (elpy-mode 1)
-;;                               (setq python-fill-docstring-style 'pep-257-nn)
-;;                               (setq python-check-command "flake8")
-;;                               (setq elpy-rpc-backend "rope")
-;; ;;                              (anaconda-mode 1)
-;;                               ))
-
-;; ;; cherry-pick some ELPY modules
-;; (setq elpy-modules '(elpy-module-company
-;;                      elpy-module-eldoc
-;;                      elpy-module-sane-defaults))
-
-;; (add-hook 'elpy-mode-hook (lambda ()
-;;                             (subword-mode 1)              ;; camelCase words
-;;                             (linum-mode 1)                ;; line numbering
-;;                             ;; (company-mode 1)              ;; auto-completion
-;;                             (rainbow-delimiters-mode 1)   ;; colored matching parens
-;;                             (elpy-use-ipython)            ;; use ipython as interpreter
-;;                             (fci-mode -1)                 ;; fill-column-indicator
-;;                             (flycheck-mode 1)
-;;                             (auto-fill-mode -1)))
-
-
-;; (setq jedi:complete-on-dot t)
+  (use-package jedi
+    :defer t
+    :init (setq jedi:complete-on-dot t)
+    :bind (("C-c d" . jedi:show-doc)))
+  
+  (add-hook 'python-mode-hook (lambda ()
+                                (elpy-enable)
+                                (elpy-mode)
+                                (subword-mode 1)
+                                (linum-mode 1)
+                                (company-mode 1)
+                                (rainbow-delimiters-mode 1)
+                                (elpy-use-ipython)
+                                (fci-mode 0)
+                                (flycheck-mode 1)
+                                (anaconda-mode 1)
+                                (auto-fill-mode 0))))
+  
 
 
 (use-package lua-mode :defer t)
-(use-package clj-refactor :defer t)
+
+(use-package yasnippet
+  :defer t
+  :diminish yas-minor-mode)
+
+(use-package eldoc
+  :defer t
+  :diminish eldoc-mode)
 
 (use-package clojure-mode
   :mode (("\\.clj\\'" . clojure-mode)
          ("\\.edn\\'" . clojure-mode))
   :init
   (use-package cider
+    :defer t
     :init
     (setq nrepl-log-messages t) ;; Log communication with the nREPL server
     (setq cider-repl-display-in-current-window t) ;; C-c C-z switch to the CIDER REPL buffer
-    (setq cider-prompt-save-file-on-load nil)
-    :config
-    (smartparens-mode 1)
-    (rainbow-delimiters-mode 1)
-    ;; (paredit-mode 0)
-    (linum-mode 1)
-    (clj-refactor-mode 1)
-    (yas-minor-mode 1) ; for adding require/use/import
-    (cljr-add-keybindings-with-prefix "C-c C-m")
-    (eldoc-mode 1)
-    (company-mode 1))
-  :config
-  (add-hook 'clojure-mode-hook (lambda () (cider-mode 1))))
+    (setq cider-prompt-save-file-on-load nil))
+  (use-package clj-refactor :defer t :diminish clj-refactor-mode)
+  (add-hook 'clojure-mode-hook (lambda ()
+                                 (cider-mode 1)
+                                 (smartparens-mode 1)
+                                 (rainbow-delimiters-mode 1)
+                                 ;; (paredit-mode 0)
+                                 (linum-mode 1)
+                                 (clj-refactor-mode 1)
+                                 (yas-minor-mode 1) ; for adding require/use/import
+                                 (cljr-add-keybindings-with-prefix "C-c C-m")
+                                 (eldoc-mode 1)
+                                 (company-mode 1))))
 
 
 
 (use-package emacs-lisp
   :mode ("\\.el\\'" . emacs-lisp-mode)
-  :config
+  :init
   (add-hook 'emacs-lisp-mode-hook (lambda ()
                                     (flycheck-mode 1)
                                     (linum-mode 1)
@@ -520,6 +487,8 @@
 (global-set-key (kbd "<f9>") 'cycle-my-theme)
 (global-set-key (kbd "C-;") 'endless/comment-line)
 (global-set-key [(hyper q)] 'save-buffers-kill-emacs)
+
+(use-package hydra :defer t)
 
 (use-package key-chord
   :init
