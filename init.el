1c96755ed10fe6e4efd09438cf3d49fad0816783
Cody Canning <cocanning11@gmail.com>
2015-08-10 15:16:19 -0400

    org-mode

--- init.el
+++ init.el
@@ -201,26 +201,34 @@
 ;;;;;;;;;;;;;;;;;;
 
 (use-package org
+  :mode ("\\.org\\'" . org-mode)
+  :init
+  (setq org-default-notes-file (concat org-directory "/notes.org"))
+  ;; fontify source blocks
+  (setq org-src-fontify-natively t)
+  ;; export to html5
+  (setq org-html-doctype "html5")
+  ;; use new fancy element types if you want
+  (setq org-html-html5-fancy t)
+  (setq org-html-postamble nil)
+  (setq org-hide-leading-stars t)
+  (setq org-capture-templates
+        '(("v" "Vessel Watcher" entry (file+headline (concat org-directory "/vessel-watcher.org") "Tasks")
+           "* TODO %?\n  %T\n  %i\n")
+          ("c" "Conflux" entry (file+headline (concat org-directory "/conflux.org") "Tasks")
+           "* TODO %?\n  %T\n  %i\n")
+          ("m" "Miscellaneous" entry (file+headline (concat org-directory "/misc.org") "Tasks")
+           "* TODO %?\n  %T\n  %i\n")
+          ("n" "Notes" entry (file+headline (concat org-directory "/notes.org") "Notes")
+           "* %?\n  %T\n")))
+
+
   :bind
   (("C-c C-r C-b" . org-bibtex-read-buffer)
    ("C-c C-w" . org-bibtex-write)
-   ("C-c a" . org-agenda)))
-
-;; (autoload 'org-mode "org.el" "Org Mode!" t)
-
-;; (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-
-;; ;; fontify source blocks
-;; (setq org-src-fontify-natively t)
-
-;; ;; export to html5
-;; (setq org-html-doctype "html5")
-
-;; ;; use new fancy element types if you want
-;; (setq org-html-html5-fancy t)
-
-;; (setq org-html-postamble nil)
-
+   ("C-c a" . org-agenda)
+   ("<f8>" . org-capture)
+   ))
 ;; (add-hook 'org-mode-hook  (lambda ()
 ;;                             (flyspell-mode 1)    
 ;;                             (visual-line-mode 1)
@@ -230,7 +238,6 @@
 
 ;; (setq org-tags-column 85)
 ;; (setq org-latex-to-pdf-process (list "latexmk -f -pdf"))
-;; (setq org-hide-leading-stars t)
 ;; (setq org-fontify-done-headline nil)
 
 ;; ;; Make Org-mode use evince in linux to open PDFs
@@ -239,10 +246,6 @@
 ;;               (lambda ()
 ;;                 (delete '("\\.pdf\\'" . default) org-file-apps)
 ;;                 (add-to-list 'org-file-apps '("\\.pdf\\'" . "evince %s")))))
-
-;; (setq org-capture-templates
-;;       '(("n" "Note" plain (file (concat org-directory "/notes.org"))
-;;          "- %?\n  %i\n")))
 
 
 (use-package markdown-mode :defer t)
