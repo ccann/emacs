86e8789d9c912b3386de1ffe3906b7b323131755
Cody Canning <cocanning11@gmail.com>
2016-08-01 10:25:24 -0400

    add forward and backward-clj-def to cider

--- functions.el
+++ functions.el
@@ -56,7 +56,7 @@ With negative prefix, apply to -N lines above."
    (line-beginning-position)
    (goto-char (line-end-position n)))
   (forward-line 1)
-  (back-to-indentation))
+    (back-to-indentation))
 
 
 (defun underline-straight ()
@@ -67,5 +67,17 @@ With negative prefix, apply to -N lines above."
     (set-face-attribute 'flycheck-error nil :underline `(:color ,curr-underline-color :style line))
     (setq curr-underline-color (cdr (assoc :color (loop for (head . tail) on (face-attribute 'flycheck-info :underline) by 'cddr collect (cons head (car tail))))))
     (set-face-attribute 'flycheck-info nil :underline `(:color ,curr-underline-color :style line))))
+
+(defun ccann/forward-clj-def nil
+  (interactive)
+  (search-forward-regexp "\n([s/]*def[n]*")
+  (recenter)
+  (move-beginning-of-line 1))
+
+(defun ccann/backward-clj-def nil
+  (interactive)
+  (search-backward-regexp "\n([s/]*def[n]*" nil 'noerror)
+  (recenter)
+  (next-line))
 
 ;;; functions.el ends here
